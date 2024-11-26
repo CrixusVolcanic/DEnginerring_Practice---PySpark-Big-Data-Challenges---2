a86a85ee27202842469e49a2f4754382b91f6447
davian_bermudez <davian.bermudez@habi.co>
2024-11-26 18:42:37 -0500

    add logger

---
+++ libraries/utils.py
@@ -0,0 +1,16 @@
+import logging
+
+def setup_logger(name, log_file, level=logging.INFO):
+
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+
+    for handler in [logging.FileHandler(log_file), logging.StreamHandler()]:
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+    return logger
+
+defaul_logger = setup_logger(name="app", log_file="default.log")
